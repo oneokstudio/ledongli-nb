429311e4b37a220b08301a3fbc874316b964d433
sysumeepo <sysumeepo@gmail.com>
2015-08-11 19:09:11 +0800

    add sql file

---
+++ vazee.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `users` (
+`id` INT(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+`phone` INT(11) NOT NULL COMMENT '手机号码',
+`user_name` VARCHAR(32) NOT NULL COMMENT '用户名字',
+`ctime` INT(11) COMMENT '操作时间',
+PRIMARY KEY (`id`, `phone`)
+) ENGINE = InnoDB COMMENT = '用户信息';
